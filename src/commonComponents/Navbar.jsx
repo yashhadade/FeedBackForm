13f205fce3c6299e4c3f770cfd6aa613efb09d6d
Yash Hadade <yashhadade13@gmail.com>
2024-11-18 13:11:50 +0530

    the project is bulid

--- src/commonComponents/Navbar.jsx
+++ src/commonComponents/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import Logo from "../assets/ismartlogo.png"
 function Navbar() {
   return (
     <header className="w-full flex items-center justify-center p-4 text-black">
       <div className="gap-5 flex flex-col  max-w-8xl w-full justify-center items-center">
         <div className="flex items-center space-x-4 w-full justify-center " >
           <div>
-          <img src="\ismartlogo.png" alt="iSmart Logo" className=" h-16 max-530:h-10 " />
+          <img src={Logo} alt="iSmart Logo" className=" h-16 max-530:h-10 " />
           </div>
           <div>
           <h1 className="font-extrabold text-3xl text-center max-530:text-2xl max-430:text-xl max-390:text-base">
