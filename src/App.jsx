4ea4db242468bc6f8dc1dc92880baf9a96e00746
Yash Hadade <yashhadade13@gmail.com>
2024-11-18 14:31:50 +0530

    remove the import in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Navbar from './commonComponents/Navbar'
-import CommanRatingField from './commonComponents/CommanRatingField'
 import RatingForm from './componentes/RatingForm'
-// import ClientSiteAdd from './componentes/ClientSiteAdd'
 import {  Route, Routes } from 'react-router-dom'
 import ClientSiteAdd from './componentes/clientSiteAdd'
 
@@ -25,14 +21,3 @@ function App() {
 }
 
 export default App
-
-
-
-
-
-
-
-
-
- 
-
