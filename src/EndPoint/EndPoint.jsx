45d31f00c2bf1a43540b254b2d45d526af709f7d
Yash Hadade <yashhadade13@gmail.com>
2024-11-19 09:59:24 +0530

    new end pint added

--- src/EndPoint/EndPoint.jsx
+++ src/EndPoint/EndPoint.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const EndPoint = () => {
-    const api="http://smartfm.ismartfacitechpl.com/feedback"
+    //const api="http://smartfm.ismartfacitechpl.com/feedback"
+    const api="http://feedback-form-app-env.eba-ufrdampp.ap-south-1.elasticbeanstalk.com"
   return api
 }
 
