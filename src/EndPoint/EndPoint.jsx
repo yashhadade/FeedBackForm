4eafa51544a8606b32c216268b30d36b0de8618b
Yash Hadade <yashhadade13@gmail.com>
2024-11-19 13:08:55 +0530

    new endpoint

--- src/EndPoint/EndPoint.jsx
+++ src/EndPoint/EndPoint.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const EndPoint = () => {
-    //const api="http://smartfm.ismartfacitechpl.com/feedback"
-    const api="http://feedback-form-app-env.eba-ufrdampp.ap-south-1.elasticbeanstalk.com"
+    const api="https://smartfm.ismartfacitechpl.com/feedback/index.php?url="
+    //const api="http://feedback-form-app-env.eba-ufrdampp.ap-south-1.elasticbeanstalk.com"
   return api
 }
 
